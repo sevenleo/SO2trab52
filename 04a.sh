e39f4f0510e3d7d011dcb2befd6e2e95694ec40d
sevenleo <enfimleo@gmail.com>
2016-01-21 02:07:01 -0200

    04a finalizado

--- 04a.sh
+++ 04a.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 clear
-#conferir print, remover primeira linha do ls
-NumArquivos=$(ls -l| wc -l)
-NumArquivos=$(($NumArquivos - 1));
 
-wc -w -l * |head -n $NumArquivos > .04atemp.txt
+#contar arquivos na pasta , removee a 1a linha de total de conferir print, remover primeira linha do ls
+NumArquivos=$(ls| wc -w)
 
 echo "		Palavras	Linhas		Arquivo"
-echo __________________________________________________________
-wc -l -c * |  sed 's/ \+/\t\t/g' | head -n $NumArquivos
 
- 
+echo __________________________________________________________
+
+echo metodo 1
+find * -maxdepth 0 -type f -exec wc -l -c {} \; |  sed 's/ \+/\t\t/g'
+
+echo __________________________________________________________
+
+echo metodo 2
+wc -l -c * 2> /dev/null |  sed 's/ \+/\t\t/g' | head -n $NumArquivos  
